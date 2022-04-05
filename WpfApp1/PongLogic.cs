4eb3ac639b03d933c443dc0328376ec85166bebb
budaidaniel <budai.daniel03@gmail.com>
2022-04-05 12:02:37 +0200

    Created Logic, and added Control enums

---
+++ WpfApp1/PongLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongGame
+{
+    public enum Controls { Left,Right}
+    class PongLogic
+    {
+
+    }
+}
