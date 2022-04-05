297627a082ae26f349c5faf5938c2a4fc7e309e2
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2022-04-05 12:00:36 +0200

    Wall done

---
+++ WpfApp1/Wall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PongGame
+{
+    class Wall : GameItem
+    {
+        private int displayWidth;
+        private int displayHeight;
+
+        public Wall(int displayWidth, int displayHeight)
+        {
+            this.displayWidth = displayWidth;
+            this.displayHeight = displayHeight;
+        }
+
+        public override Geometry Area
+        {
+            get
+            {
+                GeometryGroup gg = new GeometryGroup();
+                gg.Children.Add(new RectangleGeometry(new Rect(0, 0, displayWidth, 5)));
+                gg.Children.Add(new RectangleGeometry(new Rect(0, 0, 5, displayHeight)));
+                gg.Children.Add(new RectangleGeometry(new Rect(displayWidth - 5, 0, 5, displayHeight)));
+                gg.Children.Add(new RectangleGeometry(new Rect(0, displayHeight - 5, displayWidth, 5)));
+                return gg;
+            }
+        }
+
+    }
+}
