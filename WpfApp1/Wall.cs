57edbed1b46d468e476250d394ba6f1697ec90e9
hanptr <hanczik.peter0214@stud.uni-obuda.hu>
2022-04-05 12:18:07 +0200

    w

--- WpfApp1/Wall.cs
+++ WpfApp1/Wall.cs
@@ -11,24 +11,17 @@ namespace PongGame
     class Wall : GameItem
     {
         private int displayWidth;
-        private int displayHeight;
 
-        public Wall(int displayWidth, int displayHeight)
+        public Wall(int displayWidth)
         {
             this.displayWidth = displayWidth;
-            this.displayHeight = displayHeight;
         }
 
         public override Geometry Area
         {
             get
             {
-                GeometryGroup gg = new GeometryGroup();
-                gg.Children.Add(new RectangleGeometry(new Rect(0, 0, displayWidth, 5)));
-                gg.Children.Add(new RectangleGeometry(new Rect(0, 0, 5, displayHeight)));
-                gg.Children.Add(new RectangleGeometry(new Rect(displayWidth - 5, 0, 5, displayHeight)));
-                gg.Children.Add(new RectangleGeometry(new Rect(0, displayHeight - 5, displayWidth, 5)));
-                return gg;
+                return new RectangleGeometry(new Rect(0, 0, displayWidth, 5));
             }
         }
 
